cbcd992290af471ac59ea8479f789b0a461bd4b4
Abdullah Faqih <43257615+Kazu11@users.noreply.github.com>
2024-03-06 03:19:22 +0000

    modified:   hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import gradio as gr
 def greet(nama):
-    return "Halo " + name + ", Selamat bergabung di IBM Academy!"
+    return "Halo " + nama + ", Selamat bergabung di IBM Academy!"
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch(server_name="0.0.0.0", server_port= 7860, share=True)
